0f017527a33d728cc22e71e3787b7bbb3c0be562
Artur Brening <a.brening@gmx.de>
2018-04-18 15:47:25 +0200

    Added a basic window, working on a good layout at the moment

---
+++ src/Login.java
@@ -0,0 +1,49 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Login extends Application  {
+
+	private Button loginButton;
+	private Button registerButton;
+	private Stage loginWindow;
+	private Scene loginScene, registerScene;
+	private Label username;
+	private Label password;
+	private TextField usernameField;
+	private PasswordField passwordField;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public void start(Stage primaryStage) throws Exception {
+		
+	loginWindow = primaryStage;
+	loginWindow.setTitle("Login");
+	username = new Label("Username:");
+	password = new Label("Password:");
+	loginButton = new Button("Login");
+	registerButton = new Button("Register");
+	usernameField = new TextField();
+	passwordField = new PasswordField();
+	
+	loginButton.setOnAction(e -> loginWindow.setScene(registerScene));
+	
+	VBox loginWindowLayout = new VBox(20);
+	loginWindowLayout.getChildren().addAll(username, usernameField, password, passwordField, loginButton, registerButton);
+	
+	loginScene = new Scene(loginWindowLayout, 800, 500);
+	loginWindow.setScene(loginScene);
+	
+	loginWindow.show();
+	
+	}
+}
